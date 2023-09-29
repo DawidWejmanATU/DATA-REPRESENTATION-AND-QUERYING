79a579107be57f53b6ed7fd5fc19868502df80d6
DawidWejmanATU <G00388437@atu.ie>
2023-09-29 10:14:09 +0100

    Arrey string

--- execiseLab1.js
+++ execiseLab1.js
@@ -27,7 +27,15 @@ let Addition = (num1, num2)=>
 
 const ages = [25, 31, 42, 77];
 let ex = ages.map((item)=>{
+    if (item < 70){
+
+    
     return item * 2;
+    }
+    else 
+    {
+        return item;
+    }
 
 })
 
