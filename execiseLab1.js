bff010177874bfc2d6efb41f73e581f75175d1fb
DawidWejmanATU <G00388437@atu.ie>
2023-09-29 09:59:06 +0100

    array

--- execiseLab1.js
+++ execiseLab1.js
@@ -9,5 +9,33 @@ let value = (myvalue) =>
 {
     return myvalue;
 }
+// let numbers = (myNumbers) =>
+// {
+//     int one = 1;
+//     int two = 3;
+//     myNumbers = one + two;
+//      return myNumbers;
+// }
+// console.log(myNumbers);
+
+let Addition = (num1, num2)=>
+{
+    return num1, num2;//returning 2 numbers from the parameter 
+
+}
+
+
+const ages = [25, 31, 42, 77];
+let ex = ages.map((item)=>{
+    return item * 2;
+
+})
+
+console.log(ex);
+
+
+
+console.log(Addition(2,3));
+
 console.log(value("hellooooo"));
 DataRep();
