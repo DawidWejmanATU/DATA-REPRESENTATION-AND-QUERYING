b2ffeb518cafa6912ffe351184dcab5fdf91f508
DawidWejmanATU <G00388437@atu.ie>
2023-10-09 22:10:55 +0100

    packeges))

--- lab2/booking/src/App.js
+++ lab2/booking/src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Header from './components/header';
 import Content from './components/content';
 import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router';
 function App() {
   return (
   <BrowserRouter>
